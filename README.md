a4588f7b96b0b7b0e338ce4729329ee17a2eb594
mbilalamjad <79409563+mbilalamjad@users.noreply.github.com>
2024-04-02 01:17:20 -0700

    Grept Apply

--- README.md
+++ README.md
@@ -84,18 +84,21 @@ Default: `null`
 
 ### <a name="input_lock"></a> [lock](#input\_lock)
 
-Description: The lock level to apply to the Key Vault. Possible values are `None`, `CanNotDelete`, and `ReadOnly`.
+Description:   Controls the Resource Lock configuration for this resource. The following properties can be specified:
+
+  - `kind` - (Required) The type of lock. Possible values are `\"CanNotDelete\"` and `\"ReadOnly\"`.
+  - `name` - (Optional) The name of the lock. If not specified, a name will be generated based on the `kind` value. Changing this forces the creation of a new resource.
 
 Type:
 
 ```hcl
 object({
+    kind = string
     name = optional(string, null)
-    kind = optional(string, "None")
   })
 ```
 
-Default: `{}`
+Default: `null`
 
 ### <a name="input_public_ip_prefix_length"></a> [public\_ip\_prefix\_length](#input\_public\_ip\_prefix\_length)
 
--- main.tf
+++ main.tf
@@ -35,14 +35,14 @@ resource "azurerm_nat_gateway" "this" {
   }
 }
 
-  resource "azurerm_management_lock" "this" {
-    count = var.lock != null ? 1 : 0
-  
-    lock_level = var.lock.kind
-    name       = coalesce(var.lock.name, "lock-${var.lock.kind}")
-    scope      = azurerm_MY_RESOURCE.this.id
-    notes      = var.lock.kind == "CanNotDelete" ? "Cannot delete the resource or its child resources." : "Cannot delete or modify the resource or its child resources."
-  }
+resource "azurerm_management_lock" "this" {
+  count = var.lock != null ? 1 : 0
+
+  lock_level = var.lock.kind
+  name       = coalesce(var.lock.name, "lock-${var.lock.kind}")
+  scope      = azurerm_nat_gateway.this.id
+  notes      = var.lock.kind == "CanNotDelete" ? "Cannot delete the resource or its child resources." : "Cannot delete or modify the resource or its child resources."
+}
 
 resource "azurerm_role_assignment" "this" {
   for_each = var.role_assignments
