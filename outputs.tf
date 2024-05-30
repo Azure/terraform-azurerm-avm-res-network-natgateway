1959bdb9adbc5b55892d2e23672c29dad8762869
Bilal Amjad <79409563+mbilalamjad@users.noreply.github.com>
2024-05-29 18:23:53 -0700

    Demo (#65)
    * Description Demo
    
    * demo version
    
    * Update locals.version.tf.json
    
    * linting fix
    
    * pre-commit
    
    * Update main.tf
    
    * Update locals.version.tf.json
    
    * Update locals.version.tf.json

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,8 @@ output "resource" {
   description = "The NAT Gateway resource."
   value       = azurerm_nat_gateway.this
 }
+
+output "resource_id" {
+  description = "The ID of the NAT Gateway."
+  value       = azurerm_nat_gateway.this.id
+}
