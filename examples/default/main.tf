d7e7dfb8ddd5f06b43541d58e8f9ef0942c26c49
Jon Chancellor <jchancellor@microsoft.com>
2024-07-10 09:51:12 -0700

    added pubip and subnet support

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,25 +1,7 @@
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.7.0, < 4.0.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.5.0, < 4.0.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 # This allows us to randomize the region for the resource group.
 module "regions" {
   source  = "Azure/regions/azurerm"
-  version = ">= 0.3.0"
+  version = "~> 0.6"
 }
 
 # This allows us to randomize the region for the resource group.
@@ -34,10 +16,31 @@ module "naming" {
   version = "0.4.0"
 }
 
+locals {
+  tags = {
+    scenario = "default"
+  }
+}
+
 # This is required for resource modules
 resource "azurerm_resource_group" "this" {
   location = module.regions.regions[random_integer.region_index.result].name
   name     = module.naming.resource_group.name_unique
+}
+
+resource "azurerm_virtual_network" "this_vnet" {
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.this.location
+  name                = module.naming.virtual_network.name_unique
+  resource_group_name = azurerm_resource_group.this.name
+  tags                = local.tags
+}
+
+resource "azurerm_subnet" "this_subnet" {
+  address_prefixes     = ["10.0.1.0/24"]
+  name                 = "${module.naming.subnet.name_unique}-1"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this_vnet.name
 }
 
 # This is the module call
@@ -48,4 +51,16 @@ module "natgateway" {
   enable_telemetry    = true
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
+
+  public_ips = {
+    public_ip_1 = {
+      name = "nat_gw_pip1"
+    }
+  }
+
+  subnet_associations = {
+    subnet_1 = {
+      resource_id = azurerm_subnet.this_subnet.id
+    }
+  }
 }
